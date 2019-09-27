36b65464075ae4936a66020d7b42bac6d5bc08c0
Jonathan Cobian <jcobian@civisanalytics.com>
2019-09-26 23:51:42 -0700

    Requests: remove unused code for request bodies None of these endpoints have request bodies. All GETs

--- players.go
+++ players.go
@@ -12,7 +12,7 @@ type PlayersService struct {
 // GetAll gets all players
 func (playersService *PlayersService) GetAll(sport string) (*ObjectResponse, error) {
 	path := fmt.Sprintf("players/%s", sport)
-	req, err := playersService.client.newRequest("GET", path, nil)
+	req, err := playersService.client.newRequest("GET", path)
 
 	if err != nil {
 		return nil, err
@@ -26,7 +26,7 @@ func (playersService *PlayersService) GetAll(sport string) (*ObjectResponse, err
 // TODO: Fix this, seems to not be returning or unmarshalling right
 func (playersService *PlayersService) GetAllTrending(sport, trendingType string, lookbackHours, limit int) (*ArrayResponse, error) {
 	path := fmt.Sprintf("players/%s/trending/%s?lookback_hours=%d&limit=%d", sport, trendingType, lookbackHours, limit)
-	req, err := playersService.client.newRequest("GET", path, nil)
+	req, err := playersService.client.newRequest("GET", path)
 
 	if err != nil {
 		return nil, err
